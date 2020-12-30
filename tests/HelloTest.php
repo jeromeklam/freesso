0e2b75f85854a3f18a2b4dc3824708ebd4932aaa
jeromeklam <jeromeklam@free.fr>
2020-12-30 16:50:18 +0100

    jenkins config

---
+++ tests/HelloTest.php
@@ -0,0 +1,19 @@
+  
+<?php
+
+class HelloTest extends PHPUnit\Framework\TestCase
+{
+    
+    public function testHelloNameExample()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+}
